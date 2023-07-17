67d16791337fd2596dfe28c4b4adb125536e7438
Crowdbotics <team@crowdbotics.com>
2023-07-17 17:23:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "late-hill-42231",
+  url: "https://late-hill-42231.botics.co",
+  api: "https://late-hill-42231.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
